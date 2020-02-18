3fe5c78dec6a84716e1496d717b1b094eb070f84
llupRisingll <luisedward.miranda@gmail.com>
2020-02-18 20:19:10 +0800

    feat: Completed page UI has been added

---
+++ src/pages/Completed.jsx
@@ -0,0 +1,34 @@
+import { IonContent, IonFabButton, IonPage, IonFabList, IonToolbar, IonCard, IonItem, IonIcon, IonLabel, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonButtons, IonBackButton, IonFooter, IonTitle } from '@ionic/react';
+import React, {useState} from 'react';
+import {shuffleOutline, exitOutline} from 'ionicons/icons';
+
+const Completed = (props) => {
+
+	return (
+	<IonPage>
+		<IonContent scrollEvents={false}>
+			<div className="container">
+				<IonToolbar>
+					<IonTitle style={{color: "#63a0fc", fontSize: "3em"}}>20</IonTitle>
+				</IonToolbar>
+				<IonToolbar style={{lineHeight: "2.5em"}}>
+					<IonTitle style={{fontSize: "1.4em", lineHeight: "1.4em"}}>Cards Completed</IonTitle>
+				</IonToolbar>
+				<div style={{color: "#aaaaaa"}}>You either reshuffle or exit</div>
+			</div>
+		</IonContent>
+		<IonToolbar>
+			<div style={{width: "fit-content", margin: "20px auto"}}>
+				<IonFabButton style={{display: "inline-block", marginRight: 15}} onClick={() => props.history.push("/")}>
+					<IonIcon icon={exitOutline} style={{transform: "rotate(180deg)"}} />
+				</IonFabButton>
+				<IonFabButton color="success" style={{display: "inline-block"}} onClick={() => props.history.push("/play")} >
+					<IonIcon icon={shuffleOutline} />
+				</IonFabButton>
+			</div>
+		</IonToolbar>
+	</IonPage>
+	);
+};
+
+export default Completed;
