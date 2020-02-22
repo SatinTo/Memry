75bcd8a04731aaff70989728f45536ae7a098cf6
kimjayluta <kimjayluta0721@gmail.com>
2020-02-22 14:44:57 +0800

    chore: Changes the Year in the Footer to dynamic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,27 +10,30 @@ import {
 import React from "react";
 import "./Home.css";
 
+
 const Home = () => {
-  return (
-	<IonPage>
-		<IonContent scrollEvents={false}>
-			<div className="container">
-				<h1 style={{color: "#63a0fc", fontSize: "2.3em"}}><b>M</b>emry</h1>
-				<div>
-					<IonButton>
-						<span style={{padding:"10px 20px"}} className="textButton">Play</span>
-					</IonButton>
-					<IonButton fill="outline">
-						<span className="textButton">Set Items</span>
-					</IonButton>
-				</div>
-			</div>	
-		</IonContent>
-		<IonFooter>
-			<IonToolbar style={{textAlign:"center", color:"#9f9f9f"}}>DatosAnalyticos &copy; 2020</IonToolbar>
-		</IonFooter>
-	</IonPage>
-  );
+	const currentYear = new Date().getFullYear();
+
+	return (
+		<IonPage>
+			<IonContent scrollEvents={false}>
+				<div className="container">
+					<h1 style={{color: "#63a0fc", fontSize: "2.3em"}}><b>M</b>emry</h1>
+					<div>
+						<IonButton>
+							<span style={{padding:"10px 20px"}} className="textButton">Play</span>
+						</IonButton>
+						<IonButton fill="outline">
+							<span className="textButton">Set Items</span>
+						</IonButton>
+					</div>
+				</div>	
+			</IonContent>
+			<IonFooter>
+				<IonToolbar style={{textAlign:"center", color:"#9f9f9f"}}>DatosAnalyticos &copy; {currentYear}</IonToolbar>
+			</IonFooter>
+		</IonPage>
+	);
 };
 
 export default Home;
