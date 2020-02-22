36624b2d02baec0e4d428eb9c0b4480c0933018b
kimjayluta <kimjayluta0721@gmail.com>
2020-02-22 14:17:01 +0800

    chore: Initial UI for Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton
+} from "@ionic/react";
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-	return (
+  return (
 	<IonPage>
-		<IonHeader>
-		<IonToolbar>
-			<IonTitle>Blank</IonTitle>
-		</IonToolbar>
-		</IonHeader>
-		<IonContent>
-		<IonHeader collapse="condense">
-			<IonToolbar>
-			<IonTitle size="large">Blank</IonTitle>
-			</IonToolbar>
-		</IonHeader>
-		<ExploreContainer />
+		<IonContent scrollEvents={false}>
+			<div className="container">
+				<h1>Memry</h1>
+				<div>
+					<IonButton>Play</IonButton>
+					<IonButton fill="outline">Set Items</IonButton>
+				</div>
+			</div>
 		</IonContent>
 	</IonPage>
-	);
+  );
 };
 
 export default Home;
