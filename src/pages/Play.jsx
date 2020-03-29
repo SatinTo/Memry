a5aa6280e100f7430f5d68d34c84cc01baaa5a9b
llupRisingll <luisedward.miranda@gmail.com>
2020-03-30 01:49:40 +0800

    feat: Play section is not available.

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,16 +1,73 @@
-import { IonContent, IonFabButton, IonPage, IonToolbar, IonCard, IonIcon, IonCardSubtitle, IonCardTitle, IonCardContent, IonButtons, IonBackButton } from '@ionic/react';
 import React, {useState} from 'react';
+import { 
+	IonContent,
+	IonFabButton,
+	IonPage,
+	IonToolbar,
+	IonCard,
+	IonIcon,
+	IonCardSubtitle,
+	IonCardTitle,
+	IonCardContent,
+	IonButtons,
+	IonBackButton,
+	useIonViewWillEnter
+} from '@ionic/react';
+
 import {arrowBackOutline, refreshOutline, checkmarkDoneOutline} from 'ionicons/icons';
+import { Plugins } from '@capacitor/core';
 import './Play.css';
 
-const Play = (props) => {
+import { useHistory } from "react-router-dom";
+
+const { Storage } = Plugins;
+
+// TODO: separate into a file for pure javascript
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+	}
+	
+	return array;
+}
+
+const Play = () => {
+	const history = useHistory();
 	const [flipped, setFlip] = useState(false);
+	const [currentCardIndex, setCurrentCardIndex] = useState(0);
+	const [cardItems, setCardItems] = useState([]);
 
 	// Function to flip the card
 	function flipCard() {
 		setFlip(!flipped)
 	}
 
+	// Makesure not to cache the items
+	useIonViewWillEnter(() => {
+		console.log("here we go again");
+		Storage.get({ key: 'items' }).then((oldItems) => {
+			const oldItemsJSON = (!oldItems.value) ? [] : JSON.parse(oldItems.value);
+
+			// Put the items
+			setCardItems(shuffleArray(oldItemsJSON));
+			setCurrentCardIndex(0); // Start from top
+			setFlip(false);
+		});
+	});
+	
+	function propagate(){
+		const nextIndex = currentCardIndex+1;
+		
+		if (nextIndex >= cardItems.length){
+			history.replace("/completed/" + cardItems.length)
+			return;
+		}
+
+		setCurrentCardIndex(nextIndex);
+		setFlip(false);
+	}
+	
 	return (
 	<IonPage>
 		<IonContent scrollEvents={false}>
@@ -19,35 +76,44 @@ const Play = (props) => {
 					<IonBackButton defaultHref="home" text="" icon={arrowBackOutline} />
 				</IonButtons>
 				<div style={{display: "inline-block", marginLeft: 10, maxWidth: "85%"}}>
-					<IonCardTitle style={{fontSize: "1.2em"}}>Total Cards (100/100)</IonCardTitle>
+					<IonCardTitle style={{fontSize: "1.2em"}}>
+						Total Cards ({currentCardIndex+1}/{cardItems.length})
+					</IonCardTitle>
 					<IonCardSubtitle style={{fontWeight: "normal", textTransform: "inherit"}}>Finish all cards or press back to reshuffle</IonCardSubtitle>
 				</div>	
 			</IonToolbar>
 			<div className="container" style={{ paddingTop: "8vh"}}>
-				<IonCard style={{height: "64vh", boxShadow: "none"}}>
-					<div className={"card" + (flipped ? " is-flipped" : "")} onClick={flipCard}>
-						<div className="card__face card__face--front">
-							<IonCardContent className="container">
-								<IonCardTitle>This is the question mother fucker</IonCardTitle>
-							</IonCardContent>	
-						</div>
-						<div className="card__face card__face--back">
-							<IonCardContent className="container">
-								<IonCardTitle>This is the answer negga</IonCardTitle>
-							</IonCardContent>	
-						</div>	
-					</div>
-				</IonCard>
-				<IonToolbar>
-					<div style={{width: "fit-content", margin: "20px auto"}}>
-						<IonFabButton color="success" style={{display: "inline-block", marginRight: 15}} onClick={flipCard}>
-							<IonIcon icon={refreshOutline} />
-						</IonFabButton>
-						<IonFabButton style={{display: "inline-block"}} onClick={() => props.history.push("/completed")}>
-							<IonIcon icon={checkmarkDoneOutline} />
-						</IonFabButton>
-					</div>
-				</IonToolbar>
+				{
+					(cardItems.length < 1) ?
+						<IonCardSubtitle>Create a card to play.</IonCardSubtitle>
+					:
+					<>
+						<IonCard style={{height: "64vh", boxShadow: "none"}}>
+							<div className={"card" + (flipped ? " is-flipped" : "")} onClick={flipCard}>
+								<div className="card__face card__face--front">
+									<IonCardContent className="container">
+										<IonCardTitle>{cardItems[currentCardIndex].front}</IonCardTitle>
+									</IonCardContent>	
+								</div>
+								<div className="card__face card__face--back">
+									<IonCardContent className="container">
+										<IonCardTitle>{cardItems[currentCardIndex].back}</IonCardTitle>
+									</IonCardContent>
+								</div>	
+							</div>
+						</IonCard>
+						<IonToolbar>
+							<div style={{width: "fit-content", margin: "20px auto"}}>
+								<IonFabButton color="success" style={{display: "inline-block", marginRight: 15}} onClick={flipCard}>
+									<IonIcon icon={refreshOutline} />
+								</IonFabButton>
+								<IonFabButton style={{display: "inline-block"}} onClick={propagate}>
+									<IonIcon icon={checkmarkDoneOutline} />
+								</IonFabButton>
+							</div>
+						</IonToolbar>
+					</>
+				}
 			</div>
 		</IonContent>
 	</IonPage>
