3865c206ee69afb07cfd7df960c684c8b77d9fda
kimjayluta <kimjayluta0721@gmail.com>
2020-04-14 12:30:47 +0800

    chore: Adds the progress bar and Card counter in collection card

--- src/components/CollectionItems.jsx
+++ src/components/CollectionItems.jsx
@@ -1,28 +1,35 @@
 import {
-	IonContent,
-	IonPage,
-	IonToolbar,
-	IonIcon,
-	IonHeader,
-	IonRow,
-	IonCard,
 	IonCardContent,
 	IonCol,
-	IonCardTitle
+	IonCardTitle,
+	IonCardSubtitle,
+	IonIcon
 } from "@ionic/react";
 import React from "react";
-import {calendarOutline, folderOpenSharp, settingsOutline} from 'ionicons/icons';
+import { addSharp, albumsOutline, folderOpenOutline } from "ionicons/icons";
+// import {calendarOutline, folderOpenSharp, settingsOutline} from 'ionicons/icons';
 
 const CollectionItems = () => {
 	return (
 		<IonCol size="6">
 			<div style={{height: "20vh", boxShadow: "none"}}>
-				<div className="card__face card__face--front" style={{ borderRadius: "10px", display: "inline-block", width: "calc(100% - 10px)", margin: "auto", height: "inherit"}}>
+				<div style={{ borderRadius: "10px", display: "inline-block", width: "calc(100% - 10px)", margin: "auto", height: "inherit", backgroundColor: "#B0E7FF"}}>
 					<IonCardContent className="container">
-						<IonCardTitle style={{fontSize: "10px"}}>
+						<IonCardTitle style={{fontSize: "12px", color: "#236B8A"}}>
 							Collection 1
 						</IonCardTitle>
 					</IonCardContent>
+					<div style={{display: "flex", position: "absolute", width: "100%", bottom: "23px"}}>
+						<div style={{ display: "flex", padding: "1px", paddingRight: "20px", background: "#E5E5E5", marginLeft: "4px", borderRadius: "15px", marginTop: "4px", color:"#575757", position: "relative", zIndex: "2"}}>
+							<IonIcon icon={addSharp} style={{fontSize: "8px"}}/>
+							<span style={{fontSize:"6px", fontWeight: "bold"}}>MemPoints: 10/100</span>
+							<div style={{position: "absolute", backgroundColor: "#DD6363", width: "50%", height: "88%", borderRadius: "5px", zIndex: "-1"}}></div>
+						</div>
+						<div style={{display: "flex", marginLeft: "90px", position: "absolute", backgroundColor: "#B7B0FF", width: "30px", padding: "2px", borderRadius: "8px 0 0 8px", color: "#656290"}}>
+							<IonIcon icon={albumsOutline} style={{fontSize: "12px", paddingLeft: "4px"}} />
+							<span style={{fontSize:"10px", paddingLeft: "4px"}}>4 </span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</IonCol>
--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -4,11 +4,7 @@ import {
 	IonToolbar,
 	IonIcon,
 	IonHeader,
-	IonRow,
-	IonCard,
-	IonCardContent,
-	IonCol,
-	IonCardTitle
+	IonRow
 } from "@ionic/react";
 import React from "react";
 import {calendarOutline, folderOpenSharp, settingsOutline} from 'ionicons/icons';
@@ -21,11 +17,11 @@ const Collections = () => {
 				<IonToolbar>
 					<div slot="start" style={{ width: "80px", backgroundColor: "#FDD9A2", height: "28px", borderRadius: "5px 10px 10px 5px", marginLeft: "10px"}}>
 						<IonIcon icon={calendarOutline} style={{width: "2rem", height: "27px", color: "#D78203", float:"left"}}/>
-						<span style={{float:"left", padding: "6px", marginLeft: "10px"}}>10</span>
+						<span style={{float:"left", padding: "6px", marginLeft: "10px", fontWeight: "bold", color:"#D78203"}}>10</span>
 					</div>
 					<div style={{ width: "80px", backgroundColor: "#B0E7FF", height: "28px", borderRadius: "5px 10px 10px 5px", marginLeft: "10px"}}>
 						<IonIcon icon={folderOpenSharp} style={{width: "2rem", height: "27px", color: "#236B8A", float:"left"}}/>
-						<span style={{float:"left", padding: "6px", marginLeft: "10px"}}>10 </span>
+						<span style={{float:"left", padding: "6px", marginLeft: "10px", fontWeight: "bold", color:"#236B8A"}}>4 </span>
 					</div>
 					<IonIcon icon={settingsOutline} slot="end" style={{width: "30px", height:"30px", color:"#CCCCCC"}}/>
 				</IonToolbar>
@@ -33,6 +29,9 @@ const Collections = () => {
 			<IonContent scrollEvents={false}>
 				<div style={{padding: "0 20px"}}>
 					<IonRow>
+						<CollectionItems />
+						<CollectionItems />
+						<CollectionItems />
 						<CollectionItems />
 					</IonRow>
 				</div>
