323f1614f845bf3911f52c534bea43d62bf427f7
kimjayluta <kimjayluta0721@gmail.com>
2020-04-22 14:06:06 +0800

    chore: Fixes the bugs

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -24,7 +24,6 @@ const Item = ({id, data}) => {
 
 	return (
 		<IonCol size="6">
-			{/*  */}
 			<div style={{position: "absolute", top: "6px", right: "0", zIndex: "1"}} >
 				<IonFabButton style={{width: "30px", height:"30px", "--box-shadow": "none", "--background": "none", "--background-activated": "none"}}>
 					<IonIcon icon={ellipsisVertical} style={{height: "20px", width: "20px", color: "#000"}}/>
@@ -32,9 +31,7 @@ const Item = ({id, data}) => {
 			</div>
 
 			<IonCard className="ion-activatable ripple-parent" style={{boxShadow: "none", margin: 0}} onClick={() => {history.push("/crudCard/" + id )}}>
-	
 				<div className={"card" + (flipped ? " is-flipped" : "")} style={{paddingBottom: "152%"}}>
-					{/* Fron Card */}
 					<div className="card__face card__face--front">
 						<IonCardContent className="container">
 							<IonCardTitle style={{fontSize: "13px", color: "#656290", lineHeight: "15px"}}>
@@ -52,6 +49,7 @@ const Item = ({id, data}) => {
 				</div>
 				<IonRippleEffect type="inbound"></IonRippleEffect>
 			</IonCard>
+		
 			<div style={{position: "absolute", bottom: "9px", left: "9px"}}>
 				<IonFabButton style={{width: "20px", height:"20px", "--background": (flipped) ? "#b7b0ff" : "#97fff3"}} onClick={flipCard}>
 					<IonIcon icon={refreshOutline} style={{width: "12px", height:"12px"}} />
