e0fab0cb156d492aedf34929cce2ef0d2f944836
llupRisingll <luisedward.miranda@gmail.com>
2020-04-26 10:47:04 +0800

    chore: constant has been added

---
+++ src/vanilla/Constants.js
@@ -0,0 +1,11 @@
+
+// The list of routes
+export const PagePath = {
+	homepage: "/home",
+	play: "/play",
+	card_list: "/setItems",
+	completed: "/completed",
+	setup_card: "/crudCard",
+	collections: "/collections"
+};
+
