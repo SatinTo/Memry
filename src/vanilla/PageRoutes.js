6838d33ae1e6c75c164308b7ad3617abc0ed6459
llupRisingll <luisedward.miranda@gmail.com>
2020-07-01 07:42:40 +0800

    chore: PageRoute

---
+++ src/vanilla/PageRoutes.js
@@ -0,0 +1,11 @@
+
+// The list of routes
+export const PageRoutes = {
+	homepage: "/collections",
+	play: "/play",
+	card_list: "/cardList",
+	completed: "/completed",
+	setup_card: "/setupCard",
+	collections: "/collections"
+};
+
