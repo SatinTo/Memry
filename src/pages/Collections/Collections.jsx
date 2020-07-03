b4236c25ecfd656af2092a8d0585e8c28017b6d1
llupRisingll <luisedward.miranda@gmail.com>
2020-07-03 17:21:11 +0800

    chore: externalize the new collection hook

--- src/pages/Collections/Collections.jsx
+++ src/pages/Collections/Collections.jsx
@@ -6,49 +6,9 @@ import { Plugins } from '@capacitor/core';
 import { GlobalContext } from '../../context/GlobalStore'; 
 import RenderCollections from "../../components/RenderCollections";
 import { formatNumber } from "../../vanilla/NumberFormatter";
+import useNewCollectionPrompt from "./useNewCollectionPrompt";
 
 const { Storage } = Plugins;
-
-const useNewCollectionPrompt = () => {
-	const {dispatch} = useContext(GlobalContext);
-	
-	return function(){
-		dispatch({
-			type: "SHOW_PROMPT",
-			header: "Collection Title",
-			inputs: [{
-				name: "Title",
-				type: "text",
-				placeholder: "Enter your Collection title here..."
-			}],
-			onOkay: async (data) => {
-				// Creates new Workspace
-				const title = data.Title;
-				title.replace(/\s/g, "");
-				
-				if (title.length < 1){
-					dispatch({ type: "SHOW_TOAST", value: "Oooppss! The title should not be empty."})
-					return false;
-				}
-	
-				const collections = await Storage.get({ key: 'collections' });
-				const collectionsJson = (!collections.value || collections.value === "undefined" || !collections.hasOwnProperty) ? [] : JSON.parse(collections.value);
-	
-				const newCollections = [
-					...collectionsJson,
-					title
-				];
-	
-				await Storage.set({
-					key: 'collections',
-					value: JSON.stringify(newCollections)
-				});
-	
-				dispatch({ type: "SET_COLLECTION", value: newCollections, toast_visible: true, toast_message: "New Collection is successfully added."});
-			}
-		})
-	}
-}
 
 const useClearCollectionPrompt = (setShowpopover) => {
 	const {dispatch} = useContext(GlobalContext);
---
+++ src/pages/Collections/useNewCollectionPrompt.js
@@ -0,0 +1,46 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalStore';
+import { Storage } from '@capacitor/core';
+
+const useNewCollectionPrompt = () => {
+	const {dispatch} = useContext(GlobalContext);
+	
+	return function(){
+		dispatch({
+			type: "SHOW_PROMPT",
+			header: "Collection Title",
+			inputs: [{
+				name: "Title",
+				type: "text",
+				placeholder: "Enter your Collection title here..."
+			}],
+			onOkay: async (data) => {
+				// Creates new Workspace
+				const title = data.Title;
+				title.replace(/\s/g, "");
+				
+				if (title.length < 1){
+					dispatch({ type: "SHOW_TOAST", value: "Oooppss! The title should not be empty."})
+					return false;
+				}
+	
+				const collections = await Storage.get({ key: 'collections' });
+				const collectionsJson = (!collections.value || collections.value === "undefined" || !collections.hasOwnProperty) ? [] : JSON.parse(collections.value);
+	
+				const newCollections = [
+					...collectionsJson,
+					title
+				];
+	
+				await Storage.set({
+					key: 'collections',
+					value: JSON.stringify(newCollections)
+				});
+	
+				dispatch({ type: "SET_COLLECTION", value: newCollections, toast_visible: true, toast_message: "New Collection is successfully added."});
+			}
+		})
+	}
+}
+
+export default useNewCollectionPrompt;
