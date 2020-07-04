c4327e52c4b9e0f07a2c9fc741e25ce2b6c6b0ee
llupRisingll <luisedward.miranda@gmail.com>
2020-07-04 10:50:28 +0800

    chore: create useUpdateCollection hook

--- src/components/RenderCollections/RenderCollections.jsx
+++ src/components/RenderCollections/RenderCollections.jsx
@@ -2,15 +2,15 @@ import React, {useContext, useState} from 'react'
 import { Plugins } from '@capacitor/core';
 import CollectionItems from '../CollectionItems';
 import { GlobalContext } from '../../context/GlobalStore';
-import { useIonViewWillEnter, IonActionSheet, IonAlert } from '@ionic/react';
+import { useIonViewWillEnter, IonActionSheet } from '@ionic/react';
 import useRemoveCollectionPrompt from './useRemoveCollectionPrompt';
+import useUpdateCollectionPrompt from './useUpdateCollectionPrompt';
 
 const { Storage } = Plugins;
 
 const RenderCollections = () => {
 	const context = useContext(GlobalContext);
 	const [showActionSheet, setShowActionSheet] = useState({status: false, id: null});
-	const [showAlert, setShowAlert] = useState(false);
 	const {state: {collection}, dispatch} = context;
 
 	useIonViewWillEnter(() => {
@@ -26,52 +26,10 @@ const RenderCollections = () => {
 	});
 
 	const removeCollection = useRemoveCollectionPrompt(showActionSheet.id);
+	const updateCollection = useUpdateCollectionPrompt(showActionSheet.id);
 
 	if (collection.length < 1) {
 		return <></>
-	}
-
-	const alertProps = {
-		isOpen: showAlert,
-		onDidDismiss: () => setShowAlert(false),
-		header: "Edit Collection",
-		inputs: [
-			{
-				name: 'title',
-				type: 'text',
-				placeholder: 'Enter your collection title here...'
-			}
-		],
-		buttons: [
-			{
-				text: 'Cancel',
-				role: 'cancel',
-				cssClass: 'secondary',
-				handler: () => setShowAlert(false)
-			},
-			{
-				text: 'Ok',
-				handler: async (data) => {
-					// Edit Workspace
-					const collectionID = showActionSheet.id;
-					const newCollectionTitle = data.title;
-
-					const collections = await Storage.get({ key: 'collections' });
-					const collectionJSON = (!collections.value || collections.value === "undefined" || !collections.hasOwnProperty) ? [] : 
-					JSON.parse(collections.value)
-					
-					let updatedCollections;
-
-					if ( collectionID !== "undefined") {
-						collectionJSON[collectionID] = newCollectionTitle;
-						updatedCollections = collectionJSON;
-					}
-
-					await Storage.set({ key: 'collections', value: JSON.stringify(updatedCollections)});
-					dispatch({ type: "SET_COLLECTION", value: updatedCollections, toast_visible: true, toast_message: "Collection is successfully saved."});
-				}
-			}
-		]
 	}
 
 	return <>
@@ -79,7 +37,6 @@ const RenderCollections = () => {
 			return <CollectionItems key={index} data={data} id={index} callBack={setShowActionSheet}/>
 		})}
 
-		<IonAlert {...alertProps}/>
 		<IonActionSheet
 			isOpen={showActionSheet.status}
 			onDidDismiss={() => setShowActionSheet({status: false, id: null})}
@@ -91,7 +48,7 @@ const RenderCollections = () => {
 				}, 
 				{
 					text: 'Update',
-					handler: () => {setShowAlert(true)}
+					handler: updateCollection
 				}, 
 				{
 					text: 'Cancel',
