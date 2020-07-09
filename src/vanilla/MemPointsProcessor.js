7309e254e4ec7db1f1632e2bba8fbb3cdfd898c6
llupRisingll <luisedward.miranda@gmail.com>
2020-07-09 11:56:56 +0800

    chore: initial class for calculating the amount of mempoints on a specific collection

---
+++ src/vanilla/MemPointsProcessor.js
@@ -0,0 +1,17 @@
+
+class MemPointsProcessor {
+
+	/**
+	 * Refresh mempoints of all collectiosn or by a specific collectionID
+	 * @param {Number} collectionID 
+	 */
+	refreshMempoints(collectionID = null) {
+		let Mempoints = 0;
+
+		
+
+		return Mempoints;
+	}
+}
+
+export default MemPointsProcessor;
